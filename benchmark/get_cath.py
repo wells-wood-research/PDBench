0459d4903853e05b3b5bb678b49879bc78edcdaf
Rokas <rokas1petrenas@gmail.com>
2021-01-17 10:46:39 +0000

    Improved readability and documentation.

--- benchmark/get_cath.py
+++ benchmark/get_cath.py
@@ -1,143 +1,238 @@
+"""Functions for creating CATH datasets"""
+
 import numpy as np
 import pandas as pd
-import ampal 
+import ampal
 import gzip
 
-classes={'1'   :'Mainly Alpha',
-'2'    :'Mainly Beta',
-'3'    :'Alpha Beta',
-'4'    :'Few Secondary Structures',
-'6'    :'Special'}
+classes = {
+    "1": "Mainly Alpha",
+    "2": "Mainly Beta",
+    "3": "Alpha Beta",
+    "4": "Few Secondary Structures",
+    "6": "Special",
+}
 
-architectures={'1.10':'Orthogonal Bundle',
-'1.20':'Up-down Bundle',
-'1.25':'Alpha Horseshoe',
-'1.40':'Alpha solenoid',
-'1.50':'Alpha/alpha barrel',
-'2.10':'Ribbon',
-'2.20':'Single Sheet',
-'2.30':'Roll',
-'2.40':'Beta Barrel',
-'2.50':'Clam',
-'2.60':'Sandwich',
-'2.70':'Distorted Sandwich',
-'2.80':'Trefoil',
-'2.90':'Orthogonal Prism',
-'2.100':'Aligned Prism',
-'2.102':'3-layer Sandwich',
-'2.105':'3 Propeller',
-'2.110':'4 Propeller',
-'2.115':'5 Propeller',
-'2.120':'6 Propeller',
-'2.130':'7 Propeller',
-'2.140':'8 Propeller',
-'2.150':'2 Solenoid',
-'2.160':'3 Solenoid',
-'2.170':'Beta Complex',
-'2.180':'Shell',
-'3.10':'Roll',
-'3.15':'Super Roll',
-'3.20':'Alpha-Beta Barrel',
-'3.30':'2-Layer Sandwich',
-'3.40':'3-Layer(aba) Sandwich',
-'3.50':'3-Layer(bba) Sandwich',
-'3.55':'3-Layer(bab) Sandwich',
-'3.60':'4-Layer Sandwich',
-'3.65':'Alpha-beta prism',
-'3.70':'Box',
-'3.75':'5-stranded Propeller',
-'3.80':'Alpha-Beta Horseshoe',
-'3.90':'Alpha-Beta Complex',
-'3.100':'Ribosomal Protein L15; Chain: K; domain 2',
-'4.10':'Irregular',
-'6.10':'Helix non-globular',
-'6.20':'Other non-globular'}
+architectures = {
+    "1.10": "Orthogonal Bundle",
+    "1.20": "Up-down Bundle",
+    "1.25": "Alpha Horseshoe",
+    "1.40": "Alpha solenoid",
+    "1.50": "Alpha/alpha barrel",
+    "2.10": "Ribbon",
+    "2.20": "Single Sheet",
+    "2.30": "Roll",
+    "2.40": "Beta Barrel",
+    "2.50": "Clam",
+    "2.60": "Sandwich",
+    "2.70": "Distorted Sandwich",
+    "2.80": "Trefoil",
+    "2.90": "Orthogonal Prism",
+    "2.100": "Aligned Prism",
+    "2.102": "3-layer Sandwich",
+    "2.105": "3 Propeller",
+    "2.110": "4 Propeller",
+    "2.115": "5 Propeller",
+    "2.120": "6 Propeller",
+    "2.130": "7 Propeller",
+    "2.140": "8 Propeller",
+    "2.150": "2 Solenoid",
+    "2.160": "3 Solenoid",
+    "2.170": "Beta Complex",
+    "2.180": "Shell",
+    "3.10": "Roll",
+    "3.15": "Super Roll",
+    "3.20": "Alpha-Beta Barrel",
+    "3.30": "2-Layer Sandwich",
+    "3.40": "3-Layer(aba) Sandwich",
+    "3.50": "3-Layer(bba) Sandwich",
+    "3.55": "3-Layer(bab) Sandwich",
+    "3.60": "4-Layer Sandwich",
+    "3.65": "Alpha-beta prism",
+    "3.70": "Box",
+    "3.75": "5-stranded Propeller",
+    "3.80": "Alpha-Beta Horseshoe",
+    "3.90": "Alpha-Beta Complex",
+    "3.100": "Ribosomal Protein L15; Chain: K; domain 2",
+    "4.10": "Irregular",
+    "6.10": "Helix non-globular",
+    "6.20": "Other non-globular",
+}
+
 
 def read_data(CATH_file: str, working_dir: str) -> pd.DataFrame:
+    """If CATH .csv exists, loads the DataFrame. If CATH .txt exists, makes DataFrame and saves it.
+
+    Parameters
+    ----------
+    CATH_file: str
+        Name of CATH file.
+    working_dir: str
+        Path to CATH file.
+
+    Returns
+    -------
+    DataFrame containing CATH and PDB codes."""
+
     try:
-        df=pd.read_csv(working_dir+CATH_file+'.csv', index_col=0)
-        #start, stop needs to be str
-        df['start']=df['start'].apply(str)
-        df['stop']=df['stop'].apply(str)
-        return df
-        
-    except IOError:
-        cath_info=[]
-        temp=[]
-        start_stop=[]
-        #ftp://orengoftp.biochem.ucl.ac.uk/cath/releases/latest-release/cath-classification-data/
-        for line in open(working_dir+CATH_file+'.txt'):
-            if line[:6]=='DOMAIN': 
-                #PDB
-                temp.append(line[10:14])
-                #chain
-                temp.append(line[14])
-            if line[:6]=='CATHCO': 
-                 #class, architecture, topology, homologous superfamily
-                    cath=[int(i) for i in line[10:].strip('\n').split('.')]
-                    temp=temp+cath
-            if line[:6]=='SRANGE':
-                j=line.split()
-                #start and stop resi, can be multiple for the same chain
-                #must be str to deal with insertions (1A,1B) later.
-                start_stop.append([str(j[1][6:]),str(j[2][5:])])
-            if line[:2]=='//':
-                #keep fragments from the same chain as separate entries
-                for fragment in start_stop:
-                    cath_info.append(temp+fragment)
-                start_stop=[]
-                temp=[]
-        df=pd.DataFrame(cath_info,columns=['PDB','chain','class','architecture','topology','hsf','start', 'stop'])
-        df.to_csv(working_dir+CATH_file+'.csv')
+        df = pd.read_csv(working_dir + CATH_file + ".csv", index_col=0)
+        # start, stop needs to be str
+        df["start"] = df["start"].apply(str)
+        df["stop"] = df["stop"].apply(str)
         return df
 
-#gets fold sequence directly from PDB
-#sequences in original CATH txt file are missing uncommon residues, e.g. phosphoserine.
-#This method return normal amino acid if uncommon acid is encountered, e.g. phosposerine = S.
+    except IOError:
+        cath_info = []
+        temp = []
+        start_stop = []
+        # ftp://orengoftp.biochem.ucl.ac.uk/cath/releases/latest-release/cath-classification-data/
+        with open(working_dir + CATH_file + ".txt") as file:
+            for line in file:
+                if line[:6] == "DOMAIN":
+                    # PDB
+                    temp.append(line[10:14])
+                    # chain
+                    temp.append(line[14])
+                if line[:6] == "CATHCO":
+                    # class, architecture, topology, homologous superfamily
+                    cath = [int(i) for i in line[10:].strip("\n").split(".")]
+                    temp = temp + cath
+                if line[:6] == "SRANGE":
+                    j = line.split()
+                    # start and stop resi, can be multiple for the same chain
+                    # must be str to deal with insertions (1A,1B) later.
+                    start_stop.append([str(j[1][6:]), str(j[2][5:])])
+                if line[:2] == "//":
+                    # keep fragments from the same chain as separate entries
+                    for fragment in start_stop:
+                        cath_info.append(temp + fragment)
+                    start_stop = []
+                    temp = []
+        df = pd.DataFrame(
+            cath_info,
+            columns=[
+                "PDB",
+                "chain",
+                "class",
+                "architecture",
+                "topology",
+                "hsf",
+                "start",
+                "stop",
+            ],
+        )
+        df.to_csv(working_dir + CATH_file + ".csv")
+        return df
+
+
 def get_sequence(series: pd.Series) -> str:
+    """Gets a sequence of CATH structure segment from PDB file.
+
+    Parameters
+    ----------
+    series: pd.Series
+        Series containing one CATH instance.
+
+    Returns
+    -------
+    If PDB exists, returns sequence. If not, returns np.NaN
+
+    Notes
+    -----
+    Sequences in original CATH .txt file are missing uncommon residues, e.g. phosphoserine.
+    This method return normal amino acid if uncommon acid is encountered, e.g. phosposerine = S."""
+
     try:
-        with gzip.open('/home/shared/datasets/pdb/'+series.PDB[1:3]+'/pdb'+series.PDB+'.ent.gz','rb') as protein:
-                assembly=ampal.load_pdb(protein.read().decode(), path=False)
-                #convert pdb res id into sequence index, 
-                #some files have discontinuous residue ids so ampal.get_slice_from_res_id() does not work
-                start=0
-                stop=0
-                #if nmr structure, get 1st model
-                if isinstance(assembly,ampal.AmpalContainer):
-                    chain=assembly[0][series.chain]
+        with gzip.open(
+            "/home/shared/datasets/pdb/"
+            + series.PDB[1:3]
+            + "/pdb"
+            + series.PDB
+            + ".ent.gz",
+            "rb",
+        ) as protein:
+            assembly = ampal.load_pdb(protein.read().decode(), path=False)
+            # convert pdb res id into sequence index,
+            # some files have discontinuous residue ids so ampal.get_slice_from_res_id() does not work
+            start = 0
+            stop = 0
+            # if nmr structure, get 1st model
+            if isinstance(assembly, ampal.AmpalContainer):
+                chain = assembly[0][series.chain]
+            else:
+                chain = assembly[series.chain]
+            for i, residue in enumerate(chain):
+                # deal with insertions
+                if series.start[-1].isalpha():
+                    if (residue.id + residue.insertion_code) == series.start:
+                        start = i
                 else:
-                    chain=assembly[series.chain]
-                for i,residue in enumerate(chain):
-                    #deal with insertions
-                    if series.start[-1].isalpha():
-                        if (residue.id+residue.insertion_code)==series.start:
-                            start=i      
-                    else: 
-                        if residue.id==series.start:
-                            start=i   
-                    if series.stop[-1].isalpha():
-                        if (residue.id+residue.insertion_code)==series.stop:
-                            stop=i          
-                    else:
-                        if residue.id==series.stop:
-                            stop=i            
-        return chain[start:(stop+1)].sequence
-    #some pdbs are obsolete, return NaN
+                    if residue.id == series.start:
+                        start = i
+                if series.stop[-1].isalpha():
+                    if (residue.id + residue.insertion_code) == series.stop:
+                        stop = i
+                else:
+                    if residue.id == series.stop:
+                        stop = i
+        return chain[start : (stop + 1)].sequence
+    # some pdbs are obsolete, return NaN
     except FileNotFoundError:
         return np.NaN
 
-#return pdbs based on class, architecture, topology, homologous superfamily, at least class has to be specified
-def get_pdbs(df: pd.DataFrame, c: int, a: int=0 , t: int=0, hsf: int=0) -> pd.DataFrame:
-    if hsf!=0:
-        return df.loc[(df['class']==c) & (df['topology']==t) & (df['architecture']==a) & (df['hsf']==hsf)].copy()
-    elif t!=0:
-        return df.loc[(df['class']==c) & (df['topology']==t) & (df['architecture']==a)].copy()
-    elif a!=0:
-        return df.loc[(df['class']==c) & (df['architecture']==a)].copy()
+
+def get_pdbs(
+    df: pd.DataFrame, cls: int, arch: int = 0, topo: int = 0, homologous_sf: int = 0
+) -> pd.DataFrame:
+    """Gets PDBs based on CATH code, at least class has to be specified.
+
+    Parameters
+    ----------
+        df: pd.DataFrame
+            DataFrame containing CATH dataset.
+        cls: int
+            CATH class
+        arch: int = 0
+            CATH architecture
+        topo: int = 0
+            CATH topology
+        homologous_sf: int = 0
+            CATH homologous superfamily
+
+    Returns
+    -------
+    DataFrame containing PDBs with specified CATH code."""
+
+    if homologous_sf != 0:
+        return df.loc[
+            (df["class"] == cls)
+            & (df["topology"] == topo)
+            & (df["architecture"] == arch)
+            & (df["hsf"] == homologous_sf)
+        ].copy()
+    elif topo != 0:
+        return df.loc[
+            (df["class"] == cls)
+            & (df["topology"] == topo)
+            & (df["architecture"] == arch)
+        ].copy()
+    elif a != 0:
+        return df.loc[(df["class"] == cls) & (df["architecture"] == arch)].copy()
     else:
-        return df.loc[(df['class']==c)].copy()
-    
-#get sequences for all entries in the dataframe, appends DataFrame inplace.
-def append_sequence(df):
-    df.loc[:,'sequence']=[get_sequence(x) for i,x in df.iterrows()]
+        return df.loc[(df["class"] == cls)].copy()
+
+
+def append_sequence(df: pd.DataFrame) -> int:
+    """Get sequences for all entries in the dataframe, appends DataFrame inplace.
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        CATH dataframe
+
+    Returns
+    -------
+    Number of CATH entries for which sequence was not found."""
+
+    df.loc[:, "sequence"] = [get_sequence(x) for i, x in df.iterrows()]
     return df.sequence.isna().sum()
--- test/run_test.py
+++ test/run_test.py
@@ -1,23 +1,28 @@
 from benchmark import get_cath
 import numpy as np
+
+
 def test_load_CATH():
-    working_dir='/home/s1706179/project/sequence-recovery-benchmark/test/'
-    cath_df=get_cath.read_data('test', working_dir)
-    
-    #check shape
-    assert cath_df.shape == (11,8) , 'DataFrame shape is incorrect'
-    pdbs=get_cath.get_pdbs(cath_df,2,40,10,10)
-    assert pdbs.shape == (7,8), 'PDB shape is incorrect'
-    
-    #check pdb codes
-    assert np.all(pdbs.PDB.value_counts().values == [4, 2, 1]), 'PDBs returned incorrectly'
-    
-    #check sequence
+    working_dir = "/home/s1706179/project/sequence-recovery-benchmark/test/"
+    cath_df = get_cath.read_data("test", working_dir)
+
+    # check shape
+    assert cath_df.shape == (11, 8), "DataFrame shape is incorrect"
+    pdbs = get_cath.get_pdbs(cath_df, 2, 40, 10, 10)
+    assert pdbs.shape == (7, 8), "PDB shape is incorrect"
+
+    # check pdb codes
+    assert np.all(
+        pdbs.PDB.value_counts().values == [4, 2, 1]
+    ), "PDBs returned incorrectly"
+
+    # check sequence
     get_cath.append_sequence(pdbs)
-    assert pdbs.shape == (7,9), 'Sequence appenden incorrectly'
-    
-    example_with_insertion=get_cath.get_pdbs(cath_df,2,40,20,10)
+    assert pdbs.shape == (7, 9), "Sequence appenden incorrectly"
+
+    example_with_insertion = get_cath.get_pdbs(cath_df, 2, 40, 20, 10)
     get_cath.append_sequence(example_with_insertion)
-    assert example_with_insertion[example_with_insertion.PDB=='1cea'].sequence.values[0] == 'ECKTGNGKNYRGTMSKTKNGITCQKWSSTSPHRPRFSPATHPSEGLEENYCRNPDNDPQGPWCYTTDPEKRYDYCDILEC', 'Sequence assigned incorrectly'
-    
-  
+    assert (
+        example_with_insertion[example_with_insertion.PDB == "1cea"].sequence.values[0]
+        == "ECKTGNGKNYRGTMSKTKNGITCQKWSSTSPHRPRFSPATHPSEGLEENYCRNPDNDPQGPWCYTTDPEKRYDYCDILEC"
+    ), "Sequence assigned incorrectly"
